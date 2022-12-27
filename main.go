5d3af8e51cef1348798952acc122272599c310b9
Pavel Osminin <gauss_89@mail.ru>
2022-12-28 01:55:25 +0500

    add 'brand' struct and table in index.html

--- main.go
+++ main.go
@@ -16,6 +16,20 @@ type Vehicle struct {
 	Mileage float32 `json:"mileage"`
 }
 
+type Brand struct {
+	ID             int     `json:"id"`
+	Name           string  `json:"name"`
+	Type           string  `json:"type"`
+	SeatCapacity   string  `json:"seat_capacity"`
+	TankCapacity   float32 `json:"tank_capacity"`
+	WeightCapacity float32 `json:"weight_capacity"`
+}
+
+type VehicleBrand struct {
+	Vehicle Vehicle `json:"vehicle_entities"`
+	Brand   Brand   `json:"brand_entities"`
+}
+
 var db *sql.DB
 var err error
 
@@ -44,6 +58,32 @@ func ListVehicleHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, items)
 }
 
+func ListBrandHandler(c *gin.Context) {
+	rows, err := db.Query("SELECT * FROM brand")
+	if err != nil {
+		fmt.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "error with DB"})
+	}
+
+	items := make([]Brand, 0)
+
+	if rows != nil {
+		defer rows.Close()
+		for rows.Next() {
+
+			item := Brand{}
+			if err := rows.Scan(&item.ID, &item.Name, &item.SeatCapacity,
+				&item.TankCapacity, &item.Type, &item.WeightCapacity); err != nil {
+				fmt.Println(err.Error())
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "error with DB"})
+			}
+			items = append(items, item)
+		}
+	}
+	c.JSON(http.StatusOK, items)
+
+}
+
 func main() {
 
 	db, err = sql.Open("postgres", "postgres://postgres:postgres@localhost/vehicle?sslmode=disable")
@@ -62,6 +102,7 @@ func main() {
 
 	router.StaticFile("/", "static/index.html")
 	router.GET("/vehicles", ListVehicleHandler)
+	router.GET("/brands", ListBrandHandler)
 
 	router.Run()
 }
